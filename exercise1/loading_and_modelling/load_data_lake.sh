eae17484dc98605cf67a1333b92c90720340412a
w205 <w205@ip-172-31-3-9.ec2.internal>
2017-02-19 02:21:47 +0000

    add ddl sql statements, modified loading

--- exercise1/loading_and_modelling/load_data_lake.sh
+++ exercise1/loading_and_modelling/load_data_lake.sh
@@ -4,8 +4,13 @@ tail -n +2 Readmissions\ and\ Deaths\ -\ Hospital.csv > readmissions.csv
 tail -n +2 Measure\ Dates.csv > measures.csv
 tail -n +2 hvbp_hcahps_05_28_2015.csv > survey_responses.csv
 hdfs dfs -mkdir hospital_compare
-hdfs dfs -put hospitals.csv hospital_compare
-hdfs dfs -put effective_care.csv hospital_compare
-hdfs dfs -put readmissions.csv hospital_compare
-hdfs dfs -put measures.csv hospital_compare
-hdfs dfs -put survey_responses.csv hospital_compare
+hdfs dfs -mkdir hospital_compare/hospitals
+hdfs dfs -mkdir hospital_compare/care
+hdfs dfs -mkdir hospital_compare/readmissions
+hdfs dfs -mkdir hospital_compare/measures
+hdfs dfs -mkdir hospital_compare/surveys
+hdfs dfs -put hospitals.csv hospital_compare/hospitals
+hdfs dfs -put effective_care.csv hospital_compare/care
+hdfs dfs -put readmissions.csv hospital_compare/readmissions
+hdfs dfs -put measures.csv hospital_compare/measures
+hdfs dfs -put survey_responses.csv hospital_compare/surveys
