cef1547cb1dbac6ae6dbe00e185295eb59bbfea6
w205 <w205@ip-172-31-3-9.ec2.internal>
2017-02-19 00:16:11 +0000

    load raw data to hdfs

---
+++ exercise1/loading_and_modelling/load_data_lake.sh
@@ -0,0 +1,11 @@
+tail -n +2 Hospital\ General\ Information.csv > hospitals.csv
+tail -n +2 Timely\ and\ Effective\ Care\ -\ Hospital.csv > effective_care.csv
+tail -n +2 Readmissions\ and\ Deaths\ -\ Hospital.csv > readmissions.csv
+tail -n +2 Measure\ Dates.csv > measures.csv
+tail -n +2 hvbp_hcahps_05_28_2015.csv > survey_responses.csv
+hdfs dfs -mkdir hospital_compare
+hdfs dfs -put hospitals.csv hospital_compare
+hdfs dfs -put effective_care.csv hospital_compare
+hdfs dfs -put readmissions.csv hospital_compare
+hdfs dfs -put measures.csv hospital_compare
+hdfs dfs -put survey_responses.csv hospital_compare
