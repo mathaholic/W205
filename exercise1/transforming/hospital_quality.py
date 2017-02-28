77cdd1d0269cfdfc15758c585a8c59e89be9f0a2
w205 <w205@ip-172-31-3-9.ec2.internal>
2017-02-28 08:05:21 +0000

    update investigative analysis

--- exercise1/transforming/hospital_quality.py
+++
@@ -1,4 +0,0 @@
-from pyspark.sql import HiveContext
-sqlContext = HiveContext(sc)
-
-sqlContext.sql('show tables').show()
--- exercise1/transforming/hospital_quality.sql
+++
@@ -1,8 +0,0 @@
-drop table hospital_qual;
-
-create table hospital_qual AS
-select
-hospitals.providerid as providerid, hospitals.hospname as hospname, hospitals.hospstate as hospstate, care.condition as condition, care.measureid as measureid, care.measurename as measurename, care.score as carescore
-from hospitals
-join care on care.providerid = hospitals.providerid
-;
